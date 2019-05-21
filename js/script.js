2e7481a83827d65194ac17696f3bb372fe9342ad
dani8899 <ramdaniel593@gmail.com>
2019-05-21 19:30:14 -0400

    html skeleton is added (index.html) as well as js skeleton inside js folder

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var urlBase = "";
+var extension = "php";
+
+var userID = 0;
+var firstName = "";
+var lastName = "";
+
+function doLogin(){
+
+}
+
+
+function doRegister(){
+
+}
+
+function doLogout(){
+
+}
+
+function addContact(){
+
+}
+
+function searchContact(){
+
+}
+
+function deleteContact(){
+
+}
