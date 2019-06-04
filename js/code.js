fb6e95ae7578f1b3052181eb49bef27dd0be237a
dani8899 <ramdaniel593@gmail.com>
2019-06-03 23:56:19 -0400

    fixing search function in js

--- js/code.js
+++ js/code.js
@@ -209,21 +209,21 @@ function searchContact()
 					deleteBut.type = "button";
 					deleteBut.id = results[2] + " " + (i+1);
 					deleteBut.class = "button";
-					deleteBut.onclick = deleteContact(deleteBut.id);
+					deleteBut.setAttribute("onclick", "deleteContact(this.id);");
 					deleteBut.innerHTML = "Delete";
 
 					var editBut = document.createElement("button");
 					editBut.type = "button";
 					editBut.id = results[2] + " " + ((i+1)*2);
 					editBut.class = "button";
-					editBut.onclick = editContact(editBut.id);
+					editBut.setAttribute("onclick", "editContact(this.id);");
 					editBut.innerHTML = "Edit";
 
 					var details = document.createElement("button");
 					details.type = "button";
 					details.id = results[2] + " " + ((i+1)*3);
 					details.class = "button";
-					details.onclick = fetchContact(details.id);
+					details.setAttribute("onclick", "fetchContact(thi.id);");
 					details.innerHTML = "Details";
 
 					var resultCell = document.createElement("td");
